55ad499472b9a39329a89a9ba861ce740fccbc39
Roar Skaar <roar.skaar@gmail.com>
2020-03-21 07:19:12 +0100

    deployment: use cloud run

---
+++ .circleci/notify-slack.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+if [ -z $1 ] || [ -z $2 ]; then
+  echo "Missing input parameters required to send Slack notifications. Aborting!"
+  exit 1
+fi
+
+success=$1 # true or false
+shift
+message=${@}
+
+
+if [ "$success" = "info" ]; then
+  color="#3686A6"
+elif [ "$success" = true ]; then
+  color="#36a64f"
+else
+  color="#FF0000"
+fi
+
+# Set Slack data
+slack_data='{"attachments": [{"fallback": "'${CIRCLE_PROJECT_REPONAME}' - '${message}'","color": "'${color}'","title": "['${CIRCLE_PROJECT_REPONAME}'] - '${message}'","fields": [{"value": "'${CIRCLE_BRANCH}' / <https://circleci.com/gh/Turistforeningen/'${CIRCLE_PROJECT_REPONAME}'/'${CIRCLE_BUILD_NUM}'|'${CIRCLE_JOB}'> [<https://circleci.com/workflow-run/'${CIRCLE_WORKFLOW_ID}'|Open workflow>]","short": false}]}]}'
+
+
+echo "sending slack notification ..."
+curl -X POST $SLACK_WEBHOOK_CIRCLECI -H 'Content-type: application/json' --data "$slack_data"
+
+if [ $? != 0 ]; then
+  echo "Failed to send Slack notification!"
+  exit 1
+fi
